07793563a0d382d61555b9c304fbd95ffd6bdbd1
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2022-07-02 12:21:03 +0100

    Updates

--- src/compontents/utilities/Loading.jsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-    return ( 
-        <div className="fixed top-0 left-0 grid place-content-center w-screen h-screen bg-black text-white">
-            LOADING...
-        </div>
-    );
-}
-
-export default Loading;
